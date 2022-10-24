5fd941c180935e56d8d0e5cc1e2be6cb76e8607b
David Alexandru <fanefaraon696@gmail.com>
2022-10-25 01:04:15 +0300

    added variable declaration model

---
+++ map/lab3/untitled/src/Model/VariableDeclarationStatement.java
@@ -0,0 +1,27 @@
+package Model;
+
+import Exceptions.InterpreterException;
+import Model.ADT.Interfaces.IDictionary;
+import Model.Interfaces.IStatement;
+import Model.Value.Interfaces.IValue;
+import Model.VariablesTypes.Interfaces.IVariableType;
+
+public class VariableDeclarationStatement implements IStatement {
+    private final String name;
+    private final IValue type;
+
+    public VariableDeclarationStatement(String name, IValue type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    @Override
+    public ProgramState execute(ProgramState state) throws Exception {
+        IDictionary<String, IValue> symbolTable = state.getSymbolTable();
+        if (symbolTable.isDefined(name)){
+            throw new InterpreterException(String.format("Variable %s already defined!\n",name));
+        }
+        symbolTable.insert(name,type);
+        return state;
+    }
+}
