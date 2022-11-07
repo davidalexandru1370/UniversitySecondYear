4b3aceb62d72aed62a6005518041201b1c1536f0
David Alexandru <fanefaraon696@gmail.com>
2022-11-07 23:57:18 +0200

    added half of last subpoint lab2 hw db

--- databases/lab2/i.sql
+++ databases/lab2/i.sql
@@ -4,10 +4,20 @@
 
 use DrivingExams21;
 
-SELECT S.CNP as 'Student CNP', S.Name as 'Student Name', I.Name as 'Instructor Name' from Students S
+--get students with a instructor with expired certification
+SELECT top 5 S.CNP as 'Student CNP', S.Name as 'Student Name', I.Name as 'Instructor Name' from Students S
 INNER JOIN Instructors I ON S.InstructorCNP = I.CNP
-WHERE InstructorCNP = ANY (Select CNP from InstructorDetails Where CertificationExpiration < GETDATE())
+WHERE InstructorCNP = ANY (Select CNP from InstructorDetails ID Where CertificationExpiration < GETDATE() and ID.CNP = S.InstructorCNP)
 
-SELECT * FROM InstructorDetails ID
-INNER JOIN Instructors I on I.CNP = ID.CNP
+--get students with enough lessons for some driving licenses
+SELECT S.Name, S.CurrentLesson from Students S where CurrentLesson >= ANY(Select MIN(MandatoryLessons) from Categories)
 
+--get instructors with students who has only students who passed the practical exam from first try
+SELECT I.Name,COUNT(*) as Students FROM Instructors I
+INNER JOIN Students S ON S.InstructorCNP = I.CNP
+INNER JOIN PracticalExams P on S.CNP = P.CandidateCNP
+group by P.CandidateCNP,I.Name,S.CNP
+HAVING P.CandidateCNP = ALL(SELECT CandidateCNP from PracticalExams P2 where  P2.CandidateScore < 21)
+
+
+SELECT (Select I.Name from Instructors I where i.CNP = id.CNP) as 'Instructor Name',ID.CNP, CertificationIssued FROM InstructorDetails ID WHERE ID.CertificationIssued >= ALL(SELECT CertificationIssued FROM InstructorDetails)
