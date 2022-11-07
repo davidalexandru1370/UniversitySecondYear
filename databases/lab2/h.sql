7026713eb4633a128d915e2e4a1313ddc5affdd2
David Alexandru <fanefaraon696@gmail.com>
2022-11-07 15:18:46 +0200

    added 1 statament for point h lab2 hw db

--- databases/lab2/h.sql
+++ databases/lab2/h.sql
@@ -2,3 +2,7 @@
 --2 of the latter will also have a 
 --subquery in the HAVING clause; use the aggregation operators: 
 --COUNT, SUM, AVG, MIN, MAX;
+use DrivingExams21;
+
+SELECT  V.InstructorCNP,COUNT(V.InstructorCNP) as 'Total Cars' from Vehicles V GROUP BY V.InstructorCNP order by COUNT(V.InstructorCNP) DESC
+
