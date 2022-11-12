7666407f4021e0f5154d1c1b1c6ded0b2243f4ca
David Alexandru <fanefaraon696@gmail.com>
2022-11-12 19:34:19 +0200

    ADDED: read file concrete implementation

--- algo/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ algo/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,77 +1,103 @@
+//#include "pch.h"
 #include <iostream>
-#include <fstream>
-#include <stack>
-#include <vector>
-#include <string>
 #include <algorithm>
+#include <fstream>
+#include <vector>
+#include <climits>
+#include <queue>
+#define ll long long int
 using namespace std;
-ifstream f("ctc.in");
-ofstream o("ctc.out");
-#define NMAX 100005
-#define unvisited -1
-
-vector<int>graph[NMAX];
-int visited[NMAX] = { unvisited }, lowlink[NMAX], inStack[NMAX];
-vector<vector<int>>result;
-stack<int>connectedComponent;
-int index;
-
-void tarjan(int nod) {
-	visited[nod] = lowlink[nod] = index;
-	index++;
-
-	inStack[nod] = 1;
-	connectedComponent.push(nod);
-	for (vector<int>::iterator it = graph[nod].begin(); it != graph[nod].end(); it++) {
-		if (visited[*it] == unvisited) {
-			tarjan(*it);
-			lowlink[nod] = min(lowlink[nod], lowlink[*it]);
-		}
-		else if (inStack[*it] != 0) {
-			lowlink[nod] = min(lowlink[nod], lowlink[*it]);
-		}
+ifstream f("in.txt");
+ofstream o("o.txt");
+#define nr 1<<30
+vector<pair<int, int>>v[10005];
+int n, m, p, l, k;
+int x, y, t;
+const int MaxN = 10010;
+const int inf = 1e9 + 10;
+struct elems {
+	int source, teleports, lenght, cost;
+	bool operator<(const elems& aux) const {
+		return cost > aux.cost;
 	}
-	if (visited[nod] == lowlink[nod]) {
-		vector<int> solution;
-		int element;
-		do {
-			element = connectedComponent.top();
-			connectedComponent.pop();
-			solution.push_back(element);
-			inStack[element] = 0;
-		} while (element != nod);
-		result.push_back(solution);
+};
+priority_queue<elems> coada;
+int dp[MaxN][11][11];
+bool incoada[10005];
+vector<int>d(10005, nr);
+
+void update(int source, int teleports, int length, int cost) {
+	if (dp[source][teleports][length] > cost) {
+		dp[source][teleports][length] = cost;
+		coada.push({ source,teleports,length,cost });
 	}
 }
 
-void printResult() {
-	o << result.size() << "\n";
-	for (int line = 0; line < result.size(); line++) {
-		for (int column = 0; column < result[line].size(); column++) {
-			o << result[line][column] + 1 << " ";
+
+void dijkstra()
+{
+	for (int i = 1; i <= n; i++)
+	{
+		for (size_t j = 0; j <= k; j++)
+		{
+			for (size_t t = 0; t <= l; t++)
+			{
+				dp[i][j][t] = 1e9 + 2;
+			}
 		}
-		o << "\n";
 	}
+	dp[1][0][0] = 0;
+	coada.push({ 1,0,0,0 });
+	while (!coada.empty())
+	{
+		auto nod = coada.top();
+		coada.pop();
+		int source = nod.source;
+		int teleports = nod.teleports;
+		int length = nod.lenght;
+		int cost = nod.cost;
+		
+		/*if (dp[source][teleports][length] != cost)
+		{
+			continue;
+		}*/
+
+		if (teleports < k)
+		{
+			int cost1 = cost;
+			if (length > 0)
+			{
+				cost1 += p;
+			}
+			update(source, teleports + 1, 0, cost1);
+		}
+
+		for (auto e : v[source]) {
+			int node = e.first;
+			if (length < l) {
+				update(node, teleports, length + 1, cost);
+			}
+			if (length == 0) {
+				int cost1 = cost + e.second;
+				update(node, teleports, 0, cost1);
+			}
+		}
+	}
+
 }
+
+
 
 int main()
 {
-	int vertices, edges;
-	f >> vertices >> edges;
-	for (int it = 0; it < edges; it++) {
-		int start, end;
-		f >> start >> end;
-		graph[start - 1].push_back(end - 1);
+	f >> n >> m >> p >> l >> k;
+	for (size_t i = 1; i <= m; i++)
+	{
+		f >> x >> y >> t;
+		v[x].push_back(make_pair(y, t));
+		v[y].push_back(make_pair(x, t));
 	}
-	fill(visited, visited + NMAX, -1);
-	for (int nod = 0; nod < vertices; nod++) {
-		if (visited[nod] == unvisited) {
-			tarjan(nod);
-		}
-	}
+	dijkstra();
+	cout << dp[n][k][0];
 
-	printResult();
-
-	return 0;
 }
-
---
+++ map/lab3/untitled/src/Model/Statement/ReadFile.java
@@ -0,0 +1,56 @@
+package Model.Statement;
+
+import Exceptions.InterpreterException;
+import Model.ADT.Interfaces.IDictionary;
+import Model.ADT.Interfaces.IStack;
+import Model.Expression.Interfaces.IExpression;
+import Model.ProgramState;
+import Model.Statement.Interfaces.IStatement;
+import Model.Value.IntValue;
+import Model.Value.Interfaces.IValue;
+import Model.Value.StringValue;
+import Model.VariablesTypes.IntType;
+import Model.VariablesTypes.StringType;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.PrintWriter;
+
+public class ReadFile implements IStatement {
+    private IExpression expression;
+    private String variableName;
+
+    public ReadFile(IExpression expression, String variableName) {
+        this.expression = expression;
+        this.variableName = variableName;
+    }
+
+    @Override
+    public ProgramState execute(ProgramState state) throws Exception {
+        IStack<IStatement> stack = state.getExeStack();
+        IDictionary<String, BufferedReader> fileTable = state.getOutFiles();
+        IDictionary<String, IValue> symbolTable = state.getSymbolTable();
+        IValue value = expression.evaluate(state.getSymbolTable());
+        if (!value.getType().equals(new StringType())) {
+            throw new InterpreterException(String.format("%s is not a string", value));
+        }
+
+        if (!symbolTable.isDefined(variableName)) {
+            throw new InterpreterException(String.format("%s it is not defined!", variableName));
+        }
+        if (!symbolTable.get(variableName).getType().equals(new IntType())) {
+            throw new InterpreterException(String.format("%s is not integer", variableName));
+        }
+
+        StringValue fileName = (StringValue) value;
+        if (!fileTable.isDefined(fileName.getValue())) {
+            throw new InterpreterException(fileName + " does not exists");
+        }
+
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName.getValue()));
+
+        String line = bufferedReader.readLine();
+        symbolTable.insert(variableName, new IntValue(line.isEmpty() ? 0 : Integer.parseInt(line)));
+        return state;
+    }
+}
