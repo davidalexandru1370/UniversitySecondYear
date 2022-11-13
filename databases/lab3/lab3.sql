5c3f46fba348a8e819ac4b6da167475a407fdd71
David Alexandru <fanefaraon696@gmail.com>
2022-11-14 00:51:26 +0200

    added all statements db lab3 hw

--- databases/lab3/lab3.sql
+++ databases/lab3/lab3.sql
@@ -24,3 +24,77 @@ AS
 GO
 
 --c. add / remove a DEFAULT constraint;
+
+CREATE PROCEDURE AddDefaultToCandidateScoreInTheoreticalExams
+AS
+	alter table TheoreticalExams ADD CONSTRAINT DEFAULT_CandidateScore DEFAULT(0) for CandidateScore
+GO
+
+CREATE PROCEDURE RemoveDefaultToCandidateScoreInTheoreticalExams
+AS
+	ALTER TABLE TheoreticalExams DROP CONSTRAINT DEFAULT_CandidateScore
+GO
+
+--d. add / remove a primary key;
+CREATE PROCEDURE AddPrimaryKeyTheoreticalExams
+AS
+	ALTER TABLE TheoreticalExams add constraint PK_TheoreticalExams primary key(Id)
+GO
+
+CREATE PROCEDURE RemovePrimaryKeyTheoreticalExams
+AS
+ALTER TABLE TheoreticalExams drop CONSTRAINT PK_TheoreticalExams
+GO
+
+--e. add / remove a candidate key;
+CREATE PROCEDURE AddCarPlateUniqueInVehicles
+AS
+	ALTER TABLE Vehicles ADD CONSTRAINT CK_CarPlate_Vehicles UNIQUE (CarPlate)
+GO
+
+CREATE PROCEDURE RemoveCarPlateUniqueFromVehicles
+AS
+	ALTER TABLE VEHICLES DROP CONSTRAINT CK_CarPlate_Vehicles
+Go
+
+--f. add / remove a foreign key;
+CREATE PROCEDURE AddInstructorDetailsForeignKey
+AS
+	ALTER TABLE InstructorDetails ADD CONSTRAINT FK_InstructorDetails_CNP FOREIGN KEY(CNP) REFERENCES Instructors(CNP) ON UPDATE CASCADE ON DELETE CASCADE
+GO
+
+CREATE PROCEDURE RemoveInstructorDetailsForeignKey
+AS
+	ALTER TABLE InstructorDetails DROP CONSTRAINT FK_InstructorDetails_CNP
+GO
+
+--g. create / drop a table.
+CREATE PROCEDURE DropInstructorDetailsTable
+AS
+	drop table InstructorDetails
+GO
+
+CREATE PROCEDURE CreateInstructorDetailsTable
+AS
+	CREATE TABLE InstructorDetails(
+		CNP NVARCHAR(100),
+		CertificationIssued DATE NOT NULL,
+		CertificationExpiration DATE NOT NULL,
+		CONSTRAINT FK_InstructorDetails_CNP FOREIGN KEY(CNP) REFERENCES Instructors(CNP) ON DELETE CASCADE ON UPDATE CASCADE,
+		CONSTRAINT PK_InstructorDetails PRIMARY KEY (CNP)
+	)
+
+
+
+
+create table version (
+	version int NOT NULL
+)
+
+create table proceduresTable (
+	FromVersion INT NOT NULL,
+	ToVersion INT NOT NULL,
+	ProcedureName varchar(max),
+	CONSTRAINT PK_ProceduresTable PRIMARY KEY (FromVersion,ToVersion),
+)
+
