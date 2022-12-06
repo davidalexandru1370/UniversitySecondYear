e84b4c890ee99ad5fcec82bbf6a5eb08d1b0c72a
David Alexandru <fanefaraon696@gmail.com>
2022-12-06 14:50:14 +0200

    added

--- plf/lisp1/a.lisp
+++ plf/lisp1/a.lisp
@@ -13,8 +13,17 @@
     (twiceNThElement x 1 n)
 )
 
-(print(solve '(10 20 30 40 50) 3))
+;(print(solve '(10 20 30 40 50) 3))
+(defun testTwiceNthElement ()
+    (assert
+    (and
+        (equal '(10 20 30 30 40 50) (solve '(10 20 30 40 50) 3))
+        (equal '(10 20 30 40 50) (solve '(10 20 30 40 50) 10))
+        (equal '(10 10 20 30 40 50) (solve '(10 20 30 40 50) 1))
+    ))
+)
 
+(testTwiceNthElement)
 ;b) Write a function to return an association list with the two lists given as parameters. 
 ;Example: (A B C) (X Y Z) --> ((A.X) (B.Y) (C.Z)).
 
@@ -26,8 +35,14 @@
     )
 )
 
-(print (associationList '(A B C) '(X Y Z)))
-
+;(print (associationList '(A B C) '(X Y Z)))
+(defun testAssociationList ()
+    (assert
+    (and
+        (equal '((A . X) (B . Y) (C . Z)) (associationList '(A B C) '(X Y Z)))
+    ))
+)
+(testAssociationList)
 ;c) Write a function to determine the number of all sublists of a given list, on any level. 
 ;A sublist is either the list itself, or any element that is a list, at any level. Example: 
 ;(1 2 (3 (4 5) (6 7)) 8 (9 10)) => 5 lists:
@@ -49,8 +64,16 @@
     )
 )
 
-(print (determineNumberOfSublists '(1 2 (3 (4 5) (6 7)) 8 (9 10))))
-
+;(print (determineNumberOfSublists '(1 2 (3 (4 5) (6 7)) 8 (9 10))))
+(defun testDetermineNumberOfSublists ()
+    (assert
+    (and
+        (equal 5 (determineNumberOfSublists '(1 2 (3 (4 5) (6 7)) 8 (9 10))))
+        (equal 1 (determineNumberOfSublists '(1 2)))
+        (equal 4 (determineNumberOfSublists '(1 2 (3) (4) (5))))
+    ))
+)
+(testDetermineNumberOfSublists)
 ;d) Write a function to return the number of all numerical atoms in a list at superficial level.
 
 (defun getNumberOfNumericalAtoms (l)
@@ -61,9 +84,29 @@
     )
 )
 
-(print (getNumberOfNumericalAtoms '(1 2 (3 (4 5) (6 7)) 8 (9 10))))
+;(print (getNumberOfNumericalAtoms '(1 2 (3 (4 5) (6 7)) 8 (9 10))))
 
-;
+(defun testGetNumberOfNumericalAtoms ()
+    (assert
+    (and
+    (equal 3 (getNumberOfNumericalAtoms '(1 2 (3 (4 5) (6 7)) 8 (9 10))))
+    ))
+)
+(testGetNumberOfNumericalAtoms)
+
+(defun getNumberOfNumericalAtomsInDepth (l)
+    (cond
+    ((null l) 0)
+    ((LISTP (car l)) (+ (getNumberOfNumericalAtomsInDepth (cdr l)) (getNumberOfNumericalAtomsInDepth (car l))))
+    ((NUMBERP (car l)) (+ 1 (getNumberOfNumericalAtomsInDepth(cdr l))))
+    (T (getNumberOfNumericalAtomsInDepth(cdr l)))
+    )
+)
+
+
+(print (getNumberOfNumericalAtomsInDepth '(1 2 (3 (4 5) (6 7)) 8 (9 10))))
+
+
 
 
 ;13. For a given tree of type (2) return the path from the root node to a certain given node X.
@@ -85,6 +128,14 @@
  )
 )
 )
+
+(defun testExplorePath ()
+    (assert (and
+        (equal '(A C I J K) (explorePath '(A (B) (C (D) (E))) 0 'E))
+        )               
+    ))
+
+
 (print (explorePath '(A (B) (C (D) (E))) 0 'E ))
 (print (explorePath '(A(B(D)(E(F(G)(H))))(C(I(J(K))))) 0 'A))
 (print (explorePath '(A(B(D)(E(F(G)(H))))(C(I(J(K))))) 0 'K))
