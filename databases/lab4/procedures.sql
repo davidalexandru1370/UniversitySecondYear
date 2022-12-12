ca654188a6e762553da9e1feae882fec25a9d47b
David Alexandru <fanefaraon696@gmail.com>
2022-12-12 18:39:56 +0200

    added run test procedure for tables

--- databases/lab4/procedures.sql
+++ databases/lab4/procedures.sql
@@ -11,8 +11,6 @@
 use DrivingExams21;
 exec sp_databases;
 
-SELECT * from INFORMATION_SCHEMA.TABLES
-
 create or alter procedure insertInTables (@tableName varchar(50)) as
 	begin
 		if @tableName in (SELECT Name from Tables) begin
@@ -40,7 +38,7 @@ execute insertInTables InstructorsDrivingLicenses
 
 SELECT * from INFORMATION_SCHEMA.VIEWS;
 
-create or alter procedure insertIntoViews (@viewName varchar(50)) as
+create or alter procedure insertIntoViews (@viewName varchar(100)) as
 	begin
 		if @viewName in (SELECT Name from Views) begin
 			print CONCAT(@viewName, ' view already present in Views');
@@ -54,7 +52,7 @@ create or alter procedure insertIntoViews (@viewName varchar(50)) as
 		Insert into Views(Name) values (@viewName);
 	end
 
-create or alter procedure insertIntoTests(@testName varchar(50)) as
+create or alter procedure insertIntoTests(@testName varchar(100)) as
 	begin
 		if @testName in (SELECT Name from Tests) begin
 			print CONCAT(@testName, ' test already present in Tests');
@@ -65,7 +63,7 @@ create or alter procedure insertIntoTests(@testName varchar(50)) as
 	end
 
 
-create or alter procedure insertIntoTestTables(@testName varchar(50), @tableName varchar(50), @noOfRows int, @position int) as
+create or alter procedure insertIntoTestTables(@testName varchar(100), @tableName varchar(100), @noOfRows int, @position int) as
 	begin 
 		if @testName not in (Select Name from Tests) begin
 			print CONCAT('Does not exists a test with name = ', @testName);
@@ -102,7 +100,7 @@ create or alter procedure insertIntoTestTables(@testName varchar(50), @tableName
 	end
 
 
-create or alter procedure insertIntoTestViews(@testName varchar(50), @viewName varchar(50)) as
+create or alter procedure insertIntoTestViews(@testName varchar(100), @viewName varchar(100)) as
 	begin
 		if not exists(Select Name from Tests where Name=@testName) begin
 			print CONCAT(@testName, ' does not exists!');
@@ -119,3 +117,85 @@ create or alter procedure insertIntoTestViews(@testName varchar(50), @viewName v
 		(Select ViewId from Views where Name=@viewName)
 		)
 	end
+
+create or alter procedure runAllTests as 
+	begin
+
+		if (Select COUNT(*) from Tests) = 0 begin
+			print 'There are no tests!';
+			return;
+		end
+		
+		declare @command varchar(100);
+		declare @testStartingTime datetime2;
+		declare @individualTestStartingTime datetime2;
+		declare @individualTestEndingTime datetime2;
+		declare @table varchar(50);
+		declare @numberOfRows int;
+		declare @position int;
+		declare @view varchar(100);
+		--declare @testId int = (Select Tests.Name from Tests where Name=@testName);
+
+		declare @currentRunningTestId int;
+		declare @currentRunningTestName varchar(100);
+
+		set @currentRunningTestId = (Select max(TestRunId) from TestRunTables)
+		if @currentRunningTestId is null begin
+			set @currentRunningTestId = 0
+		end
+
+		declare tablesCursor cursor scroll for
+			SELECT T1.Name,T2.NoOfRows,T2.Position,(Select Tests.Name from Tests where Tests.TestID = T2.TestID) as TestName from Tables T1
+			inner join TestTables T2 on T1.TableId = T2.TableId
+			order by T2.Position
+
+		declare viewsCursor cursor for
+			Select V.Name
+			from Views V
+			inner join TestViews TV on V.ViewId = TV.ViewId
+		
+		set @testStartingTime = sysdatetime();
+		
+			--delete
+			open tablesCursor 
+			fetch last from tableCursor into @table, @numberOfRows, @position, @currentRunningTestName
+			while @@FETCH_STATUS = 0 begin
+				if @currentRunningTestName like '%delete'  begin
+					print CONCAT('Running ', @currentRunningTestName);
+					execute @currentRunningTestName
+					print CONCAT('Finished running test ', @currentRunningTestName);
+				end
+				fetch prior from tableCursor into @table, @numberOfRows, @position, @currentRunningTestName
+			end
+
+			close tablesCursor
+
+			open tablesCursor
+			--insert
+
+			SET IDENTITY_INSERT TestRuns ON
+			INSERT INTO TestRuns(TestRunID,Description, StartAt) values(@currentRunningTestId,'',@testStartingTime);
+			SET IDENTITY_INSERT TestRuns OFF
+			
+			fetch first from tableCursor into @table, @numberOfRows, @position, @currentRunningTestName
+			
+			while @@FETCH_STATUS = 0 begin
+				if @currentRunningTestName like 'insert%' begin
+					set @individualTestStartingTime = SYSDATETIME();
+					execute @currentRunningTestName @numberOfRows
+					set @individualTestEndingTime = SYSDATETIME();
+					INSERT INTO TestRunTables(TestRunID,TableID,StartAt,EndAt) values(@currentRunningTestId,(SELECT Tables.TableID from Tables where Tables.Name=@table),@individualTestStartingTime,@individualTestEndingTime);
+					fetch next from tablesCursor into @table, @numberOfRows, @position, @currentRunningTestName; 
+				end
+			end
+
+			close tablesCursor;
+			deallocate tablesCursor;
+			
+			--view
+
+	end
+
+
+--iterate through all testTables and start test for each Table 
+
