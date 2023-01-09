c7cefac575daa6ffddc3c4a58fbe9a6eb2165938
David <fanefaraon696@gmail.com>
2023-01-09 13:00:43 +0200

    problem2 lisp l1

--- plf/practiclisp/l1/2.lisp
+++ plf/practiclisp/l1/2.lisp
@@ -9,4 +9,24 @@
     )
 )
 
-(print (solve '((((A B) C) D E))))
+;(print (solve '((((A B) C) D E))))
+
+;c) Write a function that returns the greatest common divisor of all numbers in a nonlinear list.
+
+(defun mygcd(a b)
+    (cond
+    ((eq a b) a)
+    ((> a b) (gcd (- a b) b))
+    (t (gcd a (- b a)))
+    )
+)
+
+(defun solve2(l)
+    (cond 
+    ( (null (cdr l)) (car l))
+    ((listp (car l)) (mygcd (solve2 (cdr l)) (solve2 (car l))))
+    (t (mygcd (car l) (solve2 (cdr l))))
+    )
+)
+
+(print (solve2 '(18 3 6)))
