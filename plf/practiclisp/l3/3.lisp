9626ae6864c639870d8943991a967bd958e95974
David <fanefaraon696@gmail.com>
2023-01-15 12:16:39 +0200

    added problem 3 lisp

--- plf/practiclisp/l3/3.lisp
+++ plf/practiclisp/l3/3.lisp
@@ -1,3 +1,13 @@
-;; 3.Define   a   function   totests   the   membership   of   a   node   in   a   n-tree
+;; 3.Define   a   function   to tests   the   membership   of   a   node   in   a   n-tree
 ;; represented   as   (root list_of_nodes_subtree1 ... list_of_nodes_subtreen)
 ;;  Eg. tree is (a (b (c)) (d) (E (f))) and the node is "b" => true
+
+(defun solve(l elem)
+    (cond
+        ((equal l elem) t)
+        ((atom l) nil)
+        (t (some #'identity (mapcar #'(lambda (a) (solve a elem)) l)))
+    )
+)
+
+(print (solve '(a (b (c)) (d) (E (f))) 'z))
