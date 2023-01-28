76809cf03bf90618090391a6f764b1d481556bd2
David <fanefaraon696@gmail.com>
2023-01-28 23:08:31 +0200

    added type check for switch statement

---
+++ map/semaphore/interpretor/src/main/java/Model/Statement/SwitchStatement.java
@@ -0,0 +1,58 @@
+package Model.Statement;
+
+import Exceptions.InterpreterException;
+import Model.ADT.Interfaces.IDictionary;
+import Model.Expression.Interfaces.IExpression;
+import Model.ProgramState;
+import Model.Statement.Interfaces.IStatement;
+import Model.VariablesTypes.Interfaces.IVariableType;
+
+public class SwitchStatement implements IStatement {
+    IExpression switchExpression;
+
+    IExpression expression1;
+    IStatement statement1;
+
+    IExpression expression2;
+    IStatement statement2;
+
+    IStatement statement3;
+
+    public SwitchStatement(IExpression switchExpression,
+                           IExpression expression1,
+                           IStatement statement1,
+                           IExpression expression2,
+                           IStatement statement2,
+                           IStatement statement3) {
+        this.switchExpression = switchExpression;
+        this.expression1 = expression1;
+        this.statement1 = statement1;
+        this.expression2 = expression2;
+        this.statement2 = statement2;
+        this.statement3 = statement3;
+    }
+
+    @Override
+    public ProgramState execute(ProgramState state) throws InterpreterException {
+
+        return null;
+    }
+
+    @Override
+    public IDictionary<String, IVariableType> typeCheck(IDictionary<String, IVariableType> typeEnviroment) throws InterpreterException {
+
+        if(!(expression1.typeCheck(typeEnviroment).equals(expression2.typeCheck(typeEnviroment)) &&
+                switchExpression.typeCheck(typeEnviroment).equals(expression1.typeCheck(typeEnviroment))
+                )){
+            throw new InterpreterException("expression does not evaluate to same type");
+        }
+
+        if(!(statement1.typeCheck(typeEnviroment).equals(statement2.typeCheck(typeEnviroment)) &&
+                statement1.typeCheck(typeEnviroment).equals(statement3.typeCheck(typeEnviroment))
+        )){
+            throw new InterpreterException("statements does not evaluate to same type");
+        }
+
+        return typeEnviroment;
+    }
+}
