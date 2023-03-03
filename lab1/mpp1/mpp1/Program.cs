dcc4e62d4e8c428095316b56a01ff77edb8e4141
David <fanefaraon696@gmail.com>
2023-03-03 11:02:09 +0200

    fix new id when add vehicle

--- lab1/mpp1/mpp1/Program.cs
+++ lab1/mpp1/mpp1/Program.cs
@@ -1,4 +1,17 @@
+using mpp1.Repository;
+using mpp1.Repository.Interfaces;
+using mpp1.Service;
+using mpp1.Service.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IVehicleRepository, VehicleRepository>();
+builder.Services.AddScoped<IVehicleService, VehicleService>();
+
+
 var app = builder.Build();
 
 
@@ -8,8 +21,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseAuthorization();
 
 app.MapControllers();
 
