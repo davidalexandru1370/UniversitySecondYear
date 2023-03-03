a39cb2bfbb3ef8c12df6573e838f6eec3761ea89
David <fanefaraon696@gmail.com>
2023-03-03 10:30:03 +0200

    added vehicle interface repository

---
+++ mpp/lab1/mpp1/mpp1/Model/Vehicle.cs
@@ -0,0 +1,11 @@
+namespace mpp1.Model;
+
+public class Vehicle
+{
+    public Guid Id { get; set; }
+    public string Brand { get; set; }
+    public int HorsePower { get; set; }
+    public string? CarPlate { get; set; }
+    public int NumberOfSeats { get; set; }
+    public string OwnerName { get; set; }
+}
