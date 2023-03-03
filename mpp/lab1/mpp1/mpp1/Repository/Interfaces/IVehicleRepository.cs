ee97ce9dcc09461141c59b4079511b86a7fa3561
David <fanefaraon696@gmail.com>
2023-03-03 10:38:14 +0200

    added concrete implementation of vehicle repository

--- mpp/lab1/mpp1/mpp1/Repository/Interfaces/IVehicleRepository.cs
+++ mpp/lab1/mpp1/mpp1/Repository/Interfaces/IVehicleRepository.cs
@@ -2,11 +2,11 @@ using mpp1.Model;
 
 namespace mpp1.Repository.Interfaces;
 
-public class IVehicleRepository
+public interface IVehicleRepository
 {
     void AddVehicle(Vehicle vehicle);
     void UpdateVehicle(Guid Id, Vehicle vehicle);
     void RemoveVehicle(Guid Id);
-    Vehicle GetAllVehicles();
+    IEnumerable<Vehicle> GetAllVehicles();
     Vehicle GetVehicleById(Guid Id);
 }
--- mpp/lab1/mpp1/mpp1/Repository/VehicleRepository.cs
+++ mpp/lab1/mpp1/mpp1/Repository/VehicleRepository.cs
@@ -1,8 +1,38 @@
+using mpp1.Model;
 using mpp1.Repository.Interfaces;
 
 namespace mpp1.Repository;
 
 public class VehicleRepository : IVehicleRepository
 {
-    
+    private static List<Vehicle> vehicles = new();
+    public void AddVehicle(Vehicle vehicle)
+    {
+        vehicles.Add(vehicle);
+    }
+
+    public void UpdateVehicle(Guid Id, Vehicle vehicle)
+    {
+        var foundVehicle = vehicles.FirstOrDefault(v => v.Id == Id);
+        foundVehicle.Brand = vehicle.Brand;
+        foundVehicle.CarPlate = vehicle.CarPlate;
+        foundVehicle.NumberOfSeats = vehicle.NumberOfSeats;
+        foundVehicle.HorsePower = vehicle.HorsePower;
+        foundVehicle.OwnerName = vehicle.OwnerName;
+    }
+
+    public void RemoveVehicle(Guid Id)
+    {
+        vehicles.RemoveAll(v => v.Id == Id);
+    }
+
+    public IEnumerable<Vehicle> GetAllVehicles()
+    {
+        return vehicles;
+    }
+
+    public Vehicle GetVehicleById(Guid Id)
+    {
+        return vehicles.FirstOrDefault(v => v.Id == Id);
+    }
 }
