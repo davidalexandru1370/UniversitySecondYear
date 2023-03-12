83659cbd75829ac3e2125b9e3d24d3f7cb41fda1
David <fanefaraon696@gmail.com>
2023-03-12 19:32:42 +0200

    changed from vehicle service to client service in client controller and implemented add client endpoint

--- lab1/mpp1/mpp1/Controller/ClientController.cs
+++ lab1/mpp1/mpp1/Controller/ClientController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using mpp1.Model;
+using mpp1.Repository;
 using mpp1.Service.Interfaces;
 
 namespace mpp1.Controller;
@@ -7,11 +9,25 @@ namespace mpp1.Controller;
 [Route("api/[controller]")]
 public class ClientController : ControllerBase
 {
-    private IVehicleService _vehicleService;
+    private IClientService _clientService;
 
-    public ClientController(IVehicleService vehicleService)
+    public ClientController(IClientService clientService)
     {
-        _vehicleService = vehicleService;
+        _clientService = clientService;
+    }
+
+    [HttpPost("add-client")]
+    public async Task<ActionResult<Client>> AddClient([FromBody] Client client)
+    {
+        try
+        {
+            await _clientService.AddClient(client);
+            return Ok(client);
+        }
+        catch (RepositoryException repositoryException)
+        {
+            return BadRequest(repositoryException.Message);
+        }
     }
     
     
