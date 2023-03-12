6c6da46b9f37caebcbabf968f9c765c3a4d61e7b
David <fanefaraon696@gmail.com>
2023-03-12 16:55:37 +0200

    changed vehicle repository to database context

--- mpp/lab1/mpp1/mpp1/Repository/VehicleRepository.cs
+++ mpp/lab1/mpp1/mpp1/Repository/VehicleRepository.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using mpp1.DatabaseContext;
 using mpp1.Model;
 using mpp1.Repository.Interfaces;
 
@@ -5,35 +7,64 @@ namespace mpp1.Repository;
 
 public class VehicleRepository : IVehicleRepository
 {
-    private static List<Vehicle> _vehicles = new();
-    public void AddVehicle(Vehicle vehicle)
+    private readonly RentACarDbContext _rentACarDbContext;
+
+    public VehicleRepository(RentACarDbContext rentACarDbContext)
     {
-        _vehicles.Add(vehicle);
+        _rentACarDbContext = rentACarDbContext;
     }
 
-    public Vehicle UpdateVehicle(Vehicle vehicle)
+    public async Task AddVehicle(Vehicle vehicle)
     {
-        var foundVehicle = _vehicles.FirstOrDefault(v => v.Id == vehicle.Id);
+        await _rentACarDbContext.AddAsync(vehicle);
+        await _rentACarDbContext.SaveChangesAsync();
+    }
+
+    public async Task<Vehicle> UpdateVehicle(Vehicle vehicle)
+    {
+        var foundVehicle = await _rentACarDbContext.Vehicles.FirstOrDefaultAsync(v => v.Id == vehicle.Id);
+        if (foundVehicle is null)
+        {
+            throw new RepositoryException($"Vehicle with Id={vehicle.Id} does not exists!");
+        }
+        
         foundVehicle.Brand = vehicle.Brand;
         foundVehicle.CarPlate = vehicle.CarPlate;
         foundVehicle.NumberOfSeats = vehicle.NumberOfSeats;
         foundVehicle.HorsePower = vehicle.HorsePower;
         foundVehicle.FabricationDate = vehicle.FabricationDate;
+
+        await _rentACarDbContext.SaveChangesAsync();
         return foundVehicle;
     }
 
-    public void RemoveVehicle(Guid Id)
+    public async Task RemoveVehicle(Guid id)
     {
-        _vehicles.RemoveAll(v => v.Id == Id);
+        var vehicle = _rentACarDbContext.Vehicles.FirstOrDefault(v => v.Id == id);
+        if (vehicle is null)
+        {
+            throw new RepositoryException($"Vehicle with Id={id} does not exists!");
+        }
+       
+        _rentACarDbContext.Remove(vehicle);
+        await _rentACarDbContext.SaveChangesAsync();
     }
 
-    public IEnumerable<Vehicle> GetAllVehicles()
+    public  Task<IEnumerable<Vehicle>> GetAllVehicles()
     {
-        return _vehicles;
+        var vehicles = _rentACarDbContext.Vehicles.ToList() as IEnumerable<Vehicle>;
+        return Task.FromResult(vehicles);
     }
 
-    public Vehicle GetVehicleById(Guid Id)
+    public async Task<Vehicle> GetVehicleById(Guid id)
     {
-        return _vehicles.FirstOrDefault(v => v.Id == Id);
+        var vehicle = await _rentACarDbContext.Vehicles.FirstOrDefaultAsync(v => v.Id == id);
+
+        if (vehicle is null)
+        {
+            throw new RepositoryException($"Vehicle with Id={id} does not exists!");
+        }
+
+        return vehicle;
     }
 }
