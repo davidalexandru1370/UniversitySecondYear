c499c5588d693928a5e028dacb4a6f242b06de84
David <fanefaraon696@gmail.com>
2023-03-12 20:57:33 +0200

    new migration to fix database names

--- lab1/mpp1/mpp1/Model/Vehicle.cs
+++ lab1/mpp1/mpp1/Model/Vehicle.cs
@@ -1,11 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace mpp1.Model;
 
+[Table("Vehicle")]
 public class Vehicle
 {
+    [Key]
     public Guid Id { get; set; }
     public string Brand { get; set; }
     public int HorsePower { get; set; }
-    public string? CarPlate { get; set; }
+    public string CarPlate { get; set; }
     public int NumberOfSeats { get; set; }
     public int EngineCapacity { get; set; }
     public int FabricationDate { get; set; }
