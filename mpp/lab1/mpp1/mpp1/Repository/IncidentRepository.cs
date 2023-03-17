487bbc8dc49da6c976022b676c1db1c807fa436c
David <fanefaraon696@gmail.com>
2023-03-17 14:02:43 +0200

    added update method in controller service and repository

--- mpp/lab1/mpp1/mpp1/Repository/IncidentRepository.cs
+++ mpp/lab1/mpp1/mpp1/Repository/IncidentRepository.cs
@@ -39,6 +39,26 @@ public class IncidentRepository : IIncidentsRepository
         await _rentACarDbContext.SaveChangesAsync();
     }
 
+    public async Task<Incident> UpdateIncident(Incident incident)
+    {
+        if (incident is null)
+        {
+            throw new RepositoryException("Invalid incident");
+        }
+
+        var foundIncident = await _rentACarDbContext.Incidents.FirstOrDefaultAsync(i => i.Id == incident.Id);
+
+        if (foundIncident is null)
+        {
+            throw new RepositoryException($"Incident with Id={incident.Id} does not exists!");
+        }
+        
+        _rentACarDbContext.Entry(foundIncident).CurrentValues.SetValues(incident);
+        await _rentACarDbContext.SaveChangesAsync();
+
+        return foundIncident;
+    }
+
     public Task<IEnumerable<Incident>> GetAllIncidents()
     {
         var result = _rentACarDbContext.Set<Incident>().ToList() as IEnumerable<Incident>;
