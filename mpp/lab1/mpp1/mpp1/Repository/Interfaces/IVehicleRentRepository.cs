e69ec9a8ba828bc458237be8c3211bf218c7df2b
David <fanefaraon696@gmail.com>
2023-03-18 14:54:00 +0200

    added get vehicles by client id method in rent vehicle repository

--- mpp/lab1/mpp1/mpp1/Repository/Interfaces/IVehicleRentRepository.cs
+++ mpp/lab1/mpp1/mpp1/Repository/Interfaces/IVehicleRentRepository.cs
@@ -6,13 +6,13 @@ public interface IVehicleRentRepository
 {
     public Task AddVehicleRent(VehicleRent vehicleRent);
 
-    public Task DeleteVehicleRent(VehicleRent vehicleRent);
+    public Task DeleteVehicleRent(Guid vehicleRentId);
 
     public Task<VehicleRent> UpdateVehicleRent(VehicleRent vehicleRent);
 
-    public Task<IEnumerable<Vehicle>> GetVehiclesRentByClientId(Guid clientId);
+    public Task<IEnumerable<Vehicle>> GetVehiclesByClientId(Guid clientId);
 
-    public Task<IEnumerable<Client>> GetClientsByRentByVehicleId(Guid vehicleId);
+    public Task<IEnumerable<Client>> GetClientsByVehicleId(Guid vehicleId);
 
     public Task<VehicleRent> GetVehicleRentById(Guid vehicleRentId);
 }
---
+++ mpp/lab1/mpp1/mpp1/Repository/VehicleRentRepository.cs
@@ -0,0 +1,63 @@
+using Microsoft.EntityFrameworkCore;
+using mpp1.DatabaseContext;
+using mpp1.Model;
+using mpp1.Repository.Interfaces;
+
+namespace mpp1.Repository;
+
+public class VehicleRentRepository : IVehicleRentRepository
+{
+    private RentACarDbContext _rentACarDbContext;
+
+    public VehicleRentRepository(RentACarDbContext rentACarDbContext)
+    {
+        _rentACarDbContext = rentACarDbContext;
+    }
+
+    public async Task AddVehicleRent(VehicleRent vehicleRent)
+    {
+        _rentACarDbContext.VehicleRents.Add(vehicleRent);
+        await _rentACarDbContext.SaveChangesAsync();
+    }
+
+    public async Task DeleteVehicleRent(Guid vehicleRentId)
+    {
+        var foundVehicleRent = await GetVehicleRentById(vehicleRentId);
+        _rentACarDbContext.Remove(foundVehicleRent);
+        await _rentACarDbContext.SaveChangesAsync();
+    }
+
+    public async Task<VehicleRent> UpdateVehicleRent(VehicleRent vehicleRent)
+    {
+        if (vehicleRent is null)
+        {
+            throw new RepositoryException("Invalid vehicle rent!");
+        }
+
+        var foundVehicleRent = await GetVehicleRentById(vehicleRent.Id);
+
+        _rentACarDbContext.Entry(foundVehicleRent).CurrentValues.SetValues(vehicleRent);
+        await _rentACarDbContext.SaveChangesAsync();
+
+        return vehicleRent;
+    }
+
+    public Task<IEnumerable<Vehicle>> GetVehiclesByClientId(Guid clientId)
+    {
+        var result = _rentACarDbContext.VehicleRents
+            .Where(vr => vr.ClientId == clientId)
+            .Include(vr => vr.Vehicle)
+            as IEnumerable<Vehicle>;
+        return Task.FromResult(result);
+    }
+
+    public Task<IEnumerable<Client>> GetClientsByVehicleId(Guid vehicleId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<VehicleRent> GetVehicleRentById(Guid vehicleRentId)
+    {
+        throw new NotImplementedException();
+    }
+}
