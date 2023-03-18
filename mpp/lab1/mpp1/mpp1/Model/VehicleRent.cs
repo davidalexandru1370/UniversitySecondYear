8c46c56a0c679dfc44e00d72108c2625c35da13e
David <fanefaraon696@gmail.com>
2023-03-18 13:12:23 +0200

    added concrete implementation of VehicleRent model

--- mpp/lab1/mpp1/mpp1/Model/VehicleRent.cs
+++ mpp/lab1/mpp1/mpp1/Model/VehicleRent.cs
@@ -1,6 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
 namespace mpp1.Model;
 
 public class VehicleRent
 {
+    [Key]
+    public Guid Id { get; set; }
+    [JsonIgnore]
+    public virtual Vehicle? Vehicle { get; set; }
+    [JsonIgnore]
+    public virtual Client? Client { get; set; }
+    public virtual Guid VehicleId { get; set; }
+    public virtual Guid ClientId { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public int TotalCost { get; set; }
+    public string? Comments { get; set; }
     
 }
