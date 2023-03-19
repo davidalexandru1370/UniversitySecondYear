7d670cce907e0a98afb3c54e4e7328ee12bff2d2
David <fanefaraon696@gmail.com>
2023-03-19 13:11:22 +0200

    fixed vehicle rent get by id methods

--- lab1/mpp1/mpp1/Controller/VehicleRentController.cs
+++ lab1/mpp1/mpp1/Controller/VehicleRentController.cs
@@ -10,18 +10,18 @@ namespace mpp1.Controller;
 public class VehicleRentController : ControllerBase
 {
 
-  private IVehicleRentService _vehicleService;
+  private IVehicleRentService _vehicleRentService;
 
-  public VehicleRentController(IVehicleRentService vehicleService)
+  public VehicleRentController(IVehicleRentService vehicleRentService)
   {
-    _vehicleService = vehicleService;
+    _vehicleRentService = vehicleRentService;
   }
 
   [HttpGet]
   [Route("get-all")]
   public ActionResult<IEnumerable<VehicleRent>> GetAllRents()
   {
-    var result = _vehicleService.GetAllRents();
+    var result = _vehicleRentService.GetAllRents();
     return Ok(result);
   }
 
@@ -31,7 +31,7 @@ public class VehicleRentController : ControllerBase
   {
     try
     {
-      await _vehicleService.AddVehicleRent(vehicleRent);
+      await _vehicleRentService.AddVehicleRent(vehicleRent);
       return Ok();
     }
     catch (RepositoryException repositoryException)
@@ -42,28 +42,58 @@ public class VehicleRentController : ControllerBase
 
   [HttpGet]
   [Route("get-by-clientId/{clientId}")]
-  public ActionResult<IEnumerable<Vehicle>> GetVehiclesByClientId(Guid clientId)
+  public ActionResult<IEnumerable<Vehicle>> GetVehiclesByClientId([FromRoute]Guid clientId)
   {
-    var result = _vehicleService.GetVehiclesByClientId(clientId);
+    var result = _vehicleRentService.GetVehiclesByClientId(clientId);
     return Ok(result);
   }
 
   [HttpGet]
   [Route("get-by-vehicleId/{vehicleId}")]
-  public ActionResult<IEnumerable<Client>> GetClientsByVehicleId(Guid vehicleId)
+  public ActionResult<IEnumerable<Client>> GetClientsByVehicleId([FromRoute]Guid vehicleId)
   {
-    var result = _vehicleService.GetClientsByVehicleId(vehicleId);
+    var result = _vehicleRentService.GetClientsByVehicleId(vehicleId);
     return Ok(result);
   }
 
   [HttpDelete]
-  [Route("delete-rent/{rentId}")]
-  public async Task<IActionResult> DeleteVehicleRent(Guid vehicleRentId)
+  [Route("delete-rent/{vehicleRentId}")]
+  public async Task<IActionResult> DeleteVehicleRent([FromRoute]Guid vehicleRentId)
   {
     try
     {
-      await _vehicleService.DeleteVehicleRent(vehicleRentId);
+      await _vehicleRentService.DeleteVehicleRent(vehicleRentId);
       return Ok();
+    }
+    catch (RepositoryException repositoryException)
+    {
+      return BadRequest(repositoryException.Message);
+    }
+  }
+
+  [HttpGet]
+  [Route("get-by-id/{vehicleRentId}")]
+  public async Task<ActionResult<VehicleRent>> GetVehicleRentById([FromRoute] Guid vehicleRentId)
+  {
+    try
+    {
+      var result = await _vehicleRentService.GetVehicleRentById(vehicleRentId);
+      return Ok(result);
+    }
+    catch (RepositoryException repositoryException)
+    {
+      return BadRequest(repositoryException.Message);
+    }
+  }
+
+  [HttpPut]
+  [Route("update-vehicleRent")]
+  public async Task<ActionResult<VehicleRent>> UpdateVehicleRent([FromBody] VehicleRent vehicleRent)
+  {
+    try
+    {
+      var result = await _vehicleRentService.UpdateVehicleRent(vehicleRent);
+      return Ok(result);
     }
     catch (RepositoryException repositoryException)
     {
