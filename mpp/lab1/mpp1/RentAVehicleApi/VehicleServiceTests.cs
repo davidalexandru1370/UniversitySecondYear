2378079f7299328b6d85e639a7ec18e7e900ce33
David <fanefaraon696@gmail.com>
2023-03-25 23:50:48 +0200

    added more tests

--- mpp/lab1/mpp1/RentAVehicleApi/VehicleServiceTests.cs
+++ mpp/lab1/mpp1/RentAVehicleApi/VehicleServiceTests.cs
@@ -25,13 +25,34 @@ namespace RentAVehicleApi
                     new()
                     {
                         Brand = "BMW",
+                        HorsePower = 190,
+                        CarPlate = "SJ70RTM",
+                        NumberOfSeats = 5,
+                        EngineCapacity = 2500,
+                        FabricationDate = new DateTime(2005,03,13),
+                        NumberOfIncidents = 0
+                    },
+                    new()
+                    {
+                        Brand = "BMW",
                         HorsePower = 500,
                         CarPlate = "SJ70DAV",
                         NumberOfSeats = 5,
                         EngineCapacity = 3000,
                         FabricationDate = new DateTime(2018,03,12),
                         NumberOfIncidents = 1
-                    }
+                    },
+                    new()
+                    {
+                        Brand = "OPEL",
+                        HorsePower = 101,
+                        CarPlate = "SJ31RIA",
+                        NumberOfSeats = 5,
+                        EngineCapacity = 1900,
+                        FabricationDate = new DateTime(2003,03,13),
+                        NumberOfIncidents = 3
+                    },
+                  
                 }; 
                 
                var vehicles = new List<Vehicle>
@@ -58,6 +79,56 @@ namespace RentAVehicleApi
                             }
                         }
                     },
+                    new()
+                    {
+                        Id = Guid.Parse("0f7a7c5f-2134-42c5-b026-08db23d5f85d"),
+                        Brand = "OPEL",
+                        HorsePower = 101,
+                        CarPlate = "SJ31RIA",
+                        NumberOfSeats = 5,
+                        EngineCapacity = 1900,
+                        FabricationDate = new DateTime(2003,03,13),
+                        Incidents = new List<Incident>()
+                        {
+                            new Incident
+                            {
+                                Id = Guid.Parse("7e11e514-3b86-4685-0e80-08db255a946c"),
+                                VehicleId = Guid.Parse("0f7a7c5f-2134-42c5-b026-08db23d5f85d"),
+                                Location = "Cehu Silvaniei",
+                                Description = "gard",
+                                Cost = 800,
+                                WhenHappend = new DateTime(2019,10,11),
+                            },
+                            new Incident
+                            {
+                                Id = Guid.Parse("7e11e514-3b86-4685-0e80-08db255a956c"),
+                                VehicleId = Guid.Parse("0f7a7c5f-2134-42c5-b026-08db23d5f85d"),
+                                Location = "Cehu Silvaniei",
+                                Description = "stalp",
+                                Cost = 300,
+                                WhenHappend = new DateTime(2019,12,31),
+                            },
+                            new Incident
+                            {
+                                Id = Guid.Parse("7e11e514-3b86-4685-0e80-08db255a966c"),
+                                VehicleId = Guid.Parse("0f7a7c5f-2134-42c5-b026-08db23d5f85d"),
+                                Location = "Cehu Silvaniei",
+                                Description = "intrat in sant",
+                                Cost = 1100,
+                                WhenHappend = new DateTime(2021,11,20),
+                            }
+                        }
+                    },
+                    new()
+                    {
+                        Id = Guid.Parse("42893200-696c-4d68-38c1-08db29e7f7e3"),
+                        Brand = "BMW",
+                        HorsePower = 190,
+                        CarPlate = "SJ70RTM",
+                        NumberOfSeats = 5,
+                        EngineCapacity = 2500,
+                        FabricationDate = new DateTime(2005,03,13),
+                    }
                 };
                 
                 _vehicleRepositoryMock.Setup(v => v.GetAllVehiclesWithAllData())
@@ -65,7 +136,10 @@ namespace RentAVehicleApi
                 //Act
                 var result = await _vehicleService.GetVehiclesOrderByNumberOfIncidents();
                 //Assert
+                Assert.Equal(3,result.Count());
                 Assert.Equal(expectedResult[0],result.ToList()[0]);
+                Assert.Equal(expectedResult[1],result.ToList()[1]);
+                Assert.Equal(expectedResult[2],result.ToList()[2]);
             }
         }
 }
