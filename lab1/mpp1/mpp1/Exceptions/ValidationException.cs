0e76f33f859630acfae84a52a0b09c77cdbbf60d
David <fanefaraon696@gmail.com>
2023-03-26 23:33:06 +0300

    finished task 5

--- lab1/mpp1/mpp1/Exceptions/ValidationException.cs
+++ lab1/mpp1/mpp1/Exceptions/ValidationException.cs
@@ -1,3 +1,5 @@
+namespace mpp1.Exceptions;
+
 public class ValidationException : Exception
 {
     public ValidationException(string message) : base(message)
--- lab1/mpp1/mpp1/Model/Vehicle.cs
+++ lab1/mpp1/mpp1/Model/Vehicle.cs
@@ -9,9 +9,13 @@ public class Vehicle
     [Key]
     public Guid Id { get; set; }
     public string Brand { get; set; }
+    [Range(1,1000)]
     public int HorsePower { get; set; }
+    [RegularExpression(@"[A-Z]{2}[0-9][0-9][A-Z]{3}")]
     public string CarPlate { get; set; }
+    [Range(0,10)]
     public int NumberOfSeats { get; set; }
+    [Range(0,Int32.MaxValue)]
     public int EngineCapacity { get; set; }
     public DateTime FabricationDate { get; set; }
     public virtual ICollection<Incident>? Incidents { get; set; } = null!;
