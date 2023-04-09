96e4d2613c431527d81a723e30bbaaf2d25cfd68
David <fanefaraon696@gmail.com>
2023-04-09 13:58:48 +0300

    added pw lab5

---
+++ pw/lab5/script.js
@@ -0,0 +1,24 @@
+class Helper {
+  static index = 0;
+}
+
+$(function () {
+  $("#buttonNext").on("click", () => {
+    if (Helper.index === 4) {
+      return;
+    }
+    $(`#desktop${Helper.index}`).slideUp("slow");
+    $(`#desktop${Helper.index + 1}`).slideDown("slow");
+    Helper.index += 1;
+  });
+  $("#buttonPrevious").on("click", () => {
+    if (Helper.index === 1) {
+      return;
+    }
+
+    $(`#desktop${Helper.index - 1}`).slideDown("slow");
+    $(`#desktop${Helper.index}`).slideUp("slow");
+
+    Helper.index -= 1;
+  });
+});
