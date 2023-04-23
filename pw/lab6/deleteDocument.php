0c65b43822919e0288cea5fe35472302b5a1975e
david <davidalexandru1307@gmail.com>
2023-04-24 00:20:54 +0300

    added delete document method in php

--- pw/lab6/deleteDocument.php
+++ pw/lab6/deleteDocument.php
@@ -12,7 +12,7 @@
     <div class="container">
         <h3>Are you sure?</h3>
         <div class="inline">
-            <form action="deleteUserBackend.php" method="post">
+            <form action="deleteDocumentScript.php" method="post">
                 <input type="hidden" name="id" value="<?php echo trim($_GET['id']); ?>">
                 <button type="submit" class="button yesButton">Yes</button>
             </form>
--- pw/lab6/deleteDocumentScript.php
+++ pw/lab6/deleteDocumentScript.php
@@ -1,4 +1,16 @@
 <?php
-
-
+require_once "connection.php";
+global $connection;
+if (isset($_POST['id']) && !empty(trim($_POST['id']))) {
+    $id = $_POST['id'];
+    $sql_query = "delete from documents.document where id = '$id';";
+    $result = mysqli_query($connection, $sql_query);
+    if ($result) {
+        echo "Deleted successfully!";
+        header("Location: showAllDocuments.html");
+    } else {
+        echo "Invalid id";
+    }
+}
+mysqli_close($connection);
 ?>
