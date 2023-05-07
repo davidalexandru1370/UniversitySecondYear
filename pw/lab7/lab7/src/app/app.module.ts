b7578ad85150aa605ec492e1112e5c5a3b161937
davidalexandru1370 <fanefaraon696@gmail.com>
2023-05-07 21:31:14 +0300

    added more styling to table

--- pw/lab7/lab7/src/app/app.module.ts
+++ pw/lab7/lab7/src/app/app.module.ts
@@ -3,18 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavigationBarComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
