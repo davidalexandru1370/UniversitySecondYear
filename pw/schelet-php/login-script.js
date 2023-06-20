f22c5d96fdf80d82f64220ec5ee5210b44884712
davidalexandru1370 <fanefaraon696@gmail.com>
2023-06-20 11:01:41 +0300

    made auth to work

--- pw/schelet-php/login-script.js
+++ pw/schelet-php/login-script.js
@@ -1,24 +1,23 @@
-$( document ).ready(function() {
-    $('#loginForm').submit(function (e) {
-        e.preventDefault();
+$(document).ready(function () {
+  $("#loginForm").submit(function (e) {
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "backend/login2.php",
+      data: $(this).serialize(),
+    }).then(function (response) {
+      let data = JSON.parse(response);
+      console.log(data);
 
-        $.ajax( {
-            type: "POST",
-            url: "backend/login.php",
-            data: $(this).serialize()
-        }).then(function (response) {
-            let data = JSON.parse(response);
-            console.log(data);
-            
-            if (data.error) {
-                alert(data.error);
-                return;
-            }
+      if (data.error) {
+        alert(data.error);
+        return;
+      }
 
-            // REPLACE HERE WITH INDEX.HTML
-            location.href = "schelet-index.html";
+      // REPLACE HERE WITH INDEX.HTML
+      location.href = "schelet-index.html";
 
-            // http://localhost/schelet-php/login.html
-        });
+      // http://localhost/schelet-php/login.html
     });
+  });
 });
